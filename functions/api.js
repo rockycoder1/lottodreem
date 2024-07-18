4ddda735eab7d7b7a846aace2269d333a6c4385c
rockycsumb <rocky@solidrockdesigns.io>
2024-07-18 15:13:31 -0700

    changes

--- functions/api.js
+++ functions/api.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const router = express.Router()
 
-let records = []
+dotenv.config()
+const MONGOURL = process.env.MONGO_URL
+
+let testMongo = ''
+
+mongoose.connect(MONGOURL).then(()=> {
+    console.log('data connected')
+    testMongo = 'this worked'
+}).catch((error)=> console.log(error))
 
 //Get
 router.get('/', (req, res) => {
-    res.send('App is running...')
+    res.send('App is running...' + testMongo)
 })
 
 router.post('/add', (req,res) => {
@@ -18,6 +28,8 @@ router.post('/add', (req,res) => {
 router.delete('/', (req,res) => {
     res.send('Deleted record')
 })
+
+// console.log('hi', MONGOURL)
 
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app)
